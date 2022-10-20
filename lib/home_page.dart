c1055065ead3e78ce11c9ff062a4822f8beaf695
serhatcetin-kral <serhat06.sc@gmail.com>
2022-10-20 11:53:03 +0200

    firebase kolleksiyon verisi okuma stream ekledim

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -37,13 +37,63 @@ class _HomePageState extends State<HomePage> {
       print(list.first.data());
       print(list[1].data());
 
-
     }
-    )]
+    ),
+         //stream builder canli degistirmeyi saglar data basde birsey degisince programida degistirir
+          StreamBuilder<DocumentSnapshot>(stream:karasovalyeRef.snapshots(),builder:(BuildContext context,AsyncSnapshot asyncSnaphot)
+          {return Text("${asyncSnaphot.data.data()}");})
+          ]
         ),
       ),
     ),
     );
   }
 }
+///////////////////////////////koleksiyon verisi
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
+
+// class HomePage extends StatefulWidget {
+
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+
+//   final _firestore=FirebaseFirestore.instance;
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     CollectionReference moviesRef=_firestore.collection("MOVIES");
+
+
+//     var karasovalyeRef=moviesRef.doc("karasovalye");
+
+//     return Scaffold(
+//       backgroundColor: Colors.grey,
+//       appBar: AppBar(title: Text('firestore CRUD islemeleri'),),
+//       body: Center(
+//         child: Container(
+
+//           child: Column(children:[
+
+//     ElevatedButton(child:Text("GET QUERYSNAPSHOT"),onPressed:() async{
+//       var response=await moviesRef.get();
+//       var list=response.docs;
+//       print(list.first.data());
+//       print(list[1].data());
+
+//     }
+//     )]
+//         ),
+//       ),
+//     ),
+//     );
+//   }
+// }
