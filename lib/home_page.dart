c1bc66bca236c65f74d9155cf11ac22b9343ff9f
serhatcetin-kral <74295409+serhatcetin-kral@users.noreply.github.com>
2022-10-20 11:25:42 +0200

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -47,3 +47,56 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+
+
+///////////////////////////////koleksiyon verisi
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+
+// class HomePage extends StatefulWidget {
+
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+
+//   final _firestore=FirebaseFirestore.instance;
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     CollectionReference moviesRef=_firestore.collection("MOVIES");
+
+
+//     var karasovalyeRef=moviesRef.doc("karasovalye");
+
+//     return Scaffold(
+//       backgroundColor: Colors.grey,
+//       appBar: AppBar(title: Text('firestore CRUD islemeleri'),),
+//       body: Center(
+//         child: Container(
+
+//           child: Column(children:[
+
+//     ElevatedButton(child:Text("GET QUERYSNAPSHOT"),onPressed:() async{
+//       var response=await moviesRef.get();
+//       var list=response.docs;
+//       print(list.first.data());
+//       print(list[1].data());
+
+//     }
+//     )]
+//         ),
+//       ),
+//     ),
+//     );
+//   }
+// }
+
+
+
