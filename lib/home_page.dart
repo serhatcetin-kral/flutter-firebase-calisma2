c8df5f12eca480a135c47fa66e642b916cc27cbb
serhatcetin-kral <74295409+serhatcetin-kral@users.noreply.github.com>
2022-10-21 11:18:01 +0200

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -61,6 +61,12 @@ class _HomePageState extends State<HomePage> {
                           child: ListTile(
                             title:Text("${listOfDocumentSnapshot[index].get('name')}",style: TextStyle(fontSize: 24),),
                             subtitle:  Text("${listOfDocumentSnapshot[index].get('year')}",style: TextStyle(fontSize: 16),),
+                            trailing: IconButton(icon: Icon(Icons.delete),
+                              onPressed: () async{
+                              //fire basede data silmek icin  delete metodu
+                                await listOfDocumentSnapshot[index].reference.delete();
+
+                              },),
 
                           ),
                         );
@@ -85,6 +91,95 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+////////////////////yukaridaki kisim delete butonlari eklenmis hali
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+
+// class HomePage extends StatefulWidget {
+
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+
+//   final _firestore=FirebaseFirestore.instance;
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     CollectionReference moviesRef=_firestore.collection("MOVIES");
+
+
+//     var karasovalyeRef=moviesRef.doc("karasovalye");
+
+//     return Scaffold(
+//       backgroundColor: Colors.grey,
+//       appBar: AppBar(title: Text('firestore CRUD islemeleri'),),
+//       body: Center(
+//         child: Container(
+
+//           child: Column(children:[
+
+//             // ElevatedButton(child:Text("GET QUERYSNAPSHOT"),onPressed:() async{
+//             //   var response=await moviesRef.get();
+//             //   var list=response.docs;
+//             //   print(list.first.data());
+//             //   print(list[1].data());
+//             //
+//             // }
+//             // ),
+//             //stream builder canli degistirmeyi saglar data basde birsey degisince programida degistirir
+//             StreamBuilder<QuerySnapshot>(
+//                 stream:moviesRef.snapshots(),builder:(BuildContext context,AsyncSnapshot asyncSnaphot)
+//             {
+
+
+
+//               if(asyncSnaphot.hasError){
+//                 return Center(child: Text("some error is occured"),);
+//               }
+//               else{
+//                 if(asyncSnaphot.hasData){
+//                   List<DocumentSnapshot> listOfDocumentSnapshot=asyncSnaphot.data.docs;
+//                   return  Flexible(
+//                     child: ListView.builder(
+//                       itemCount: listOfDocumentSnapshot.length,
+//                       itemBuilder: (context,index){
+
+//                         return Card(
+//                           child: ListTile(
+//                             title:Text("${listOfDocumentSnapshot[index].get('name')}",style: TextStyle(fontSize: 24),),
+//                             subtitle:  Text("${listOfDocumentSnapshot[index].get('year')}",style: TextStyle(fontSize: 16),),
+
+//                           ),
+//                         );
+
+
+//                       },
+//                     ),
+//                   );
+//                 }
+//                 else{
+
+//                   return Center(child: CircularProgressIndicator(),);
+//                 }
+//               }
+
+
+//             })
+//           ]
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 
